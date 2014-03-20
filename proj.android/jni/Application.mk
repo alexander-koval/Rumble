bdb06f6d9463bcd1a691b5c763345bcfde6b6d97
skincat <kobamail.88@gmail.com>
2014-03-21 01:13:34 +0800

    little fixes

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -3,6 +3,6 @@ APP_ABI			:=	armeabi-v7a
 #APP_ABI			:= x86
 APP_STL 		:= gnustl_static
 APP_CFLAGS		:= -g -ggdb -O0
-APP_CPPFLAGS 	:= -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -DCOCOS2D_DEBUG=1 -std=c++11
+APP_CPPFLAGS 	:= -frtti -DCC_ENABLE_BOX2D_INTEGRATION=1 -DCOCOS2D_DEBUG=1 -std=c++11
 APP_OPTIM		:= debug
 
